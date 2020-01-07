21dc8f13557b7dbe9f8d11a169988dde01cbbf00
praveenautomation-eng <praveennarra123@gmail.com>
2020-01-07 15:13:13 +0530

    newfile

---
+++ selenium projects/src/com/Praveen/FirstClass.java
@@ -0,0 +1,20 @@
+package com.Praveen;
+
+public class FirstClass {
+	
+
+	public static void main(String[] args) {
+		
+		
+		System.out.println("enter a name");
+		System.out.println("push and commit ");
+		System.out.println("added code");
+		System.out.println("  praveen");
+	}
+}
+
+
+
+
+
+
---
+++ selenium projects/src/com/practice/C.java
@@ -0,0 +1,23 @@
+package com.practice;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class C extends Methods
+{
+	
+	
+	
+	public static void main(String[] args) throws Exception {
+	
+		proload();
+		launch("chromebrowser");
+		
+		navigateurl("amazomurl");
+		}
+	
+	
+	
+
+}
